8614eebfabfeab024b1675a49f6162387a7d86b3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-19 06:49:42 -0300

    build: add new depedencys jna, docker-java, apache-components

--- src/main/java/com/daniel/docker_manager/DockerManagerApplication.java
+++ src/main/java/com/daniel/docker_manager/DockerManagerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerManagerApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DockerManagerApplication.class, args);
 	}
