c2762268a215bdd85d9fd60ff8804d8d90c8ca50
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-20 10:12:25 -0300

    feat: creating design to header dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/Header";
+
 export default () => {
     return (
         <>
+            <Header />
             <div>
                 <h1>Criando o front end da aplicação</h1>
             </div>
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default () => {
+    return (
+        <header>
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                fazendo um teste
+            </nav>
+        </header>
+    );
+};
