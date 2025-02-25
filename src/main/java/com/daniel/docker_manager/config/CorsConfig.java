f0febee2e02fd9ef12401e61b25ae3e7a3346aa7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-25 17:50:08 -0300

    bug: trying a new method to resolve change some files

--- src/main/java/com/daniel/docker_manager/config/CorsConfig.java
+++ src/main/java/com/daniel/docker_manager/config/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://frontend:5173", "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
